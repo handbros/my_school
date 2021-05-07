a2f5c2ebef181fcc49c27e05c1d2ca923c170e80
Syri <junimiso04@naver.com>
2021-05-08 02:30:17 +0900

    App icon is added.

---
+++ lib/SharedAssets.g.dart
@@ -0,0 +1,36 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'SharedAssets.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SharedAssets _$SharedAssetsFromJson(Map<String, dynamic> json) {
+  return SharedAssets(
+    acceptUsingDeviceStorage: json['acceptUsingDeviceStorage'] as bool,
+    useOfflineMode: json['useOfflineMode'] as bool,
+    acceptTransferringDeviceInformation:
+        json['acceptTransferringDeviceInformation'] as bool,
+    classSearchHistory:
+        (json['classSearchHistory'] as List)?.map((e) => e as String)?.toList(),
+    classList: (json['classList'] as List)
+        ?.map((e) =>
+            e == null ? null : ClassInfo.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    selectedClass: json['selectedClass'] == null
+        ? null
+        : ClassInfo.fromJson(json['selectedClass'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$SharedAssetsToJson(SharedAssets instance) =>
+    <String, dynamic>{
+      'acceptUsingDeviceStorage': instance.acceptUsingDeviceStorage,
+      'useOfflineMode': instance.useOfflineMode,
+      'acceptTransferringDeviceInformation':
+          instance.acceptTransferringDeviceInformation,
+      'classSearchHistory': instance.classSearchHistory,
+      'classList': instance.classList?.map((e) => e?.toJson())?.toList(),
+      'selectedClass': instance.selectedClass?.toJson(),
+    };
